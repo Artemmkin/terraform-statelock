6dd95d0a2e4c0d5b17234d661766dc2f376324ac
Artemmkin <artemstarostenko65@gmail.com>
2018-02-27 14:56:40 -0800

    Update dynamodb.tf

--- terraform-required/dynamodb.tf
+++ terraform-required/dynamodb.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "terraform_statelock" {
   name           = "terraform-statelock"
   read_capacity  = "5"
-  write_capacity = "10"
+  write_capacity = "5"
   hash_key       = "LockID"              # The table must have a primary key named LockID for state locking
 
   attribute {
